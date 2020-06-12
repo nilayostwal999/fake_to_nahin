7fbc1aac9bee8c17bf9b5ab5a81c737f7e6cc233
Nilay Ostwal <57761082+nilayostwal999@users.noreply.github.com>
2020-06-12 14:42:32 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,72 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-void main(){
-  runApp( faketonahin() );
+import 'util/drawer.dart' as drew;
+import 'screen/homepage.dart' as homepage;
+import 'screen/newsview.dart' as newsview;
+
+void main() {
+  runApp(MyApp());
 }
 
-class faketonahin extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Fake To Nahin",
+      title: 'Fake To Nahin',
       theme: ThemeData(
         primarySwatch: Colors.green,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Expanded(flex: 1,child:
-              Column(children: [
-                IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.menu, color: Colors.white,),
-                ),
-              ],),),
-              Expanded(flex: 5,child:
-              Column(children: [
-                Title(color: Colors.white, child: Text("Fake To Nahin", style: TextStyle(fontWeight: FontWeight.w800),))
-              ],crossAxisAlignment: CrossAxisAlignment.end,),),
-              Expanded(flex: 3,child:
-              Column(children: [
-                RaisedButton(
-                  onPressed: (){},
-                  child: Row(
-                    children:[
-                      Icon(Icons.add, color: Colors.white,),
-                      Text("New Post", style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white))
-                    ],mainAxisAlignment: MainAxisAlignment.end,mainAxisSize: MainAxisSize.min,
-                  ),
-                  color: Colors.green, elevation: 10.0,
-                ),
-              ],crossAxisAlignment: CrossAxisAlignment.end,),),
-            ],mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ),
-        body: Column(
-          children:[
-            buildSizedBox()
-          ]
-        )
-        
-      ),
+      home: MyHomePage(title: 'Fake To Nahin'),
     );
   }
+}
 
-  SizedBox buildSizedBox() {
-    return SizedBox(
-        
-        height: 120,
-        child: RaisedButton(
-          onPressed: (){},
-          child: Row(
-            children: [
-              SizedBox(
-                width:100.0,
-                child:Column(
-                  children: [
-                    Image.network("https://www.indoindians.com/wp-content/uploads/2015/12/learning-768x516.jpg", height: 120.0,)
-                  ],
-                ) 
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(children: [
-                      Text('Heading', style: TextStyle(fontWeight: FontWeight.w600,fontSize: 24.0,decoration: TextDecoration.underline),)
-                    ]),
-                    
-                    Row(mainAxisAlignment: MainAxisAlignment.start,children: [
-                        Text('Description:', style: TextStyle(fontWeight: FontWeight.w400,fontSize: 20.0,),),
-                    ],),
-                    Row(children: [
-                      Text('Description Provided', style: TextStyle(color:Colors.grey,fontWeight: FontWeight.w400,fontSize: 14.0,),overflow: TextOverflow.fade,),
-                    ]) 
-                    
-                    ],)
-                ),
-              SizedBox(
-                width: 80,
-                child: Column(
-                  children:[
-                    Text('Unique\nLinks\nAvailable:',style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w400,decoration: TextDecoration.underline),textAlign: TextAlign.center,),
-                  ]
-                ),
-              )
-            ],
-          ),
-          color: Colors.white,splashColor: Colors.green,highlightColor: Colors.grey,elevation: 10.0,
-        )
-      );
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      drawer: new drew.Drawer_button(),
+      appBar: new AppBar(
+        centerTitle: true,
+        title: new Expanded(flex:1,child: Row(children:[Text('Fake To Nahin',style:TextStyle(fontWeight:FontWeight.bold,fontSize: 24),textAlign: TextAlign.center,)])),
+        actions: [
+          RaisedButton(onPressed: (){},
+            child: Row(children:[Icon(Icons.add),Text('New Post')]),
+            textColor: Colors.white,
+            color: Colors.green,
+            splashColor: Colors.white,
+          )
+        ],
+      ),
+      body: stat(),      ); // This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
+
+class stat extends StatefulWidget {
+  @override
+  _statState createState() => _statState();
+}
+class _statState extends State<stat>{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      verticalDirection: VerticalDirection.down,
+      children:[
+        newsview.news_view()
+      ]
+    );
   }
 }
---
+++ lib/screen/profileview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class profilev extends StatefulWidget {
+  @override
+  _profilevState createState() => _profilevState();
+}
+
+class _profilevState extends State<profilev> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
