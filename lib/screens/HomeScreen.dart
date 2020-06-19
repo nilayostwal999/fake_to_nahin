3165cb4bb7e23de6ff4e800b12bfdaf9f56fbaf8
nilayostwal999 <nilay.ostwal999@gmail.com>
2020-06-20 04:46:28 +0530

    homescreen code optimized

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -16,7 +16,6 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       appBar: AppBar(
         title: Text(
-          
           'Fake To Nahin',
           style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
@@ -60,152 +59,83 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildPostCard(BuildContext context, DocumentSnapshot document) {
     return Card(
-        child: InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, 'Post');
-            },
-            child: DecoratedBox(
-                position: DecorationPosition.background,
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(2),
-                    border: Border.all(
-                        color: Colors.grey,
-                        width: 2,
-                        style: BorderStyle.values[1])),
-                child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Column(children: [
-                      RichText(
-                          text: TextSpan(
-                              text: document['title'],
-                              style: TextStyle(
-                                  fontSize: 26,
-                                  color: Colors.lightBlue[800],
-                                  fontWeight: FontWeight.bold))),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text( document['username'],
-                              style: TextStyle(
-                                  fontSize: 22, fontWeight: FontWeight.bold)),
-                          Text(document['dateCreated'],
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.grey))
-                        ],
-                      ),
-                      FractionallySizedBox(
-                          widthFactor: 0.95,
-                          child: Image.network(
-                            document['imagePath'],fit: BoxFit.fitWidth,
-                          )),
-                      Text("Description\n",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 23,
-                              fontWeight: FontWeight.bold)),
-                      RichText(
-                          overflow: TextOverflow.fade,
-                          maxLines: 3,
-                          text: TextSpan(
-                              text: document['description'],
-                              style: TextStyle(
-                                  fontSize: 18, color: Colors.black))),
-                      Text('Read More and View Resources',
-                          style: TextStyle(
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline))
-                    ])))));
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, 'Post');
+        },
+        child: DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(2),
+            border: Border.all(
+              color: Colors.grey,
+              width: 2,
+              style: BorderStyle.values[1]
+            )
+          ),
+          child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                RichText(
+                  text: TextSpan(
+                    text: document['title'],
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.lightBlue[800],
+                      fontWeight: FontWeight.bold
+                    )
+                  )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text( document['username'],
+                      style: TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold
+                      )
+                    ),
+                    Text(document['dateCreated'],
+                      style:
+                        TextStyle(fontSize: 20, color: Colors.grey
+                      )
+                    )
+                  ],
+                ),
+                FractionallySizedBox(
+                  widthFactor: 0.95,
+                  child: Image.network(
+                    document['imagePath'],fit: BoxFit.fitWidth,
+                  )
+                ),
+                Text("Description\n",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+                RichText(
+                  overflow: TextOverflow.fade,
+                  maxLines: 3,
+                  text: TextSpan(
+                    text: document['description'],
+                    style: TextStyle(
+                      fontSize: 18, color: Colors.black
+                    )
+                  )
+                ),
+                Text('Read More and View Resources',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline
+                  )
+                )
+              ]
+            )
+          )
+        )
+      )
+    );
   }
 }
-
-// SizedBox(
-//             height: 120,
-//             child: RaisedButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                       fullscreenDialog: true,
-//                       maintainState: true,
-//                       builder: (context) => NewsView
-//                           .news_view()), // builder of MaterialPageRoute will call the TodoDetail class passing the todo that was passed.
-//                 );
-//               },
-//               child: Row(
-//                 children: [
-//                   SizedBox(
-//                       width: 100.0,
-//                       child: Column(
-//                         children: [
-//                           Image.network(
-//                             "https://www.indoindians.com/wp-content/uploads/2015/12/learning-768x516.jpg",
-//                             height: 120.0,
-//                           )
-//                         ],
-//                       )),
-//                   Expanded(
-//                       child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: [
-//                       Row(children: [
-//                         RichText(
-//                             text: TextSpan(
-//                           text: "heading",
-//                           style: TextStyle(
-//                               color: Colors.black,
-//                               fontWeight: FontWeight.w600,
-//                               fontSize: 24.0,
-//                               decoration: TextDecoration.underline),
-//                         ))
-//                       ]),
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.start,
-//                         children: [
-//                           Text(
-//                             'Description:',
-//                             style: TextStyle(
-//                               fontWeight: FontWeight.w400,
-//                               fontSize: 20.0,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                       Row(children: [
-//                         Text(
-//                           "description",
-//                           style: TextStyle(
-//                             color: Colors.grey,
-//                             fontWeight: FontWeight.w400,
-//                             fontSize: 14.0,
-//                           ),
-//                           overflow: TextOverflow.fade,
-//                         ),
-//                       ])
-//                     ],
-//                   )),
-//                   SizedBox(
-//                     width: 80,
-//                     child: Column(children: [
-//                       Text(
-//                         'Unique\nLinks\nAvailable:\n',
-//                         style: TextStyle(
-//                             fontSize: 18.0,
-//                             fontWeight: FontWeight.w400,
-//                             decoration: TextDecoration.underline),
-//                         textAlign: TextAlign.center,
-//                       ),
-//                       Text(
-//                         "links",
-//                         style: TextStyle(
-//                             fontSize: 24, fontWeight: FontWeight.bold),
-//                       )
-//                     ]),
-//                   )
-//                 ],
-//               ),
-//               color: Colors.white,
-//               splashColor: Colors.green,
-//               highlightColor: Colors.grey,
-//               elevation: 10.0,
-//             ))
